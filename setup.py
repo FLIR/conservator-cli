e045740c7d48ce425b7f6de15ae72b849d56e165
Ron Lockwood-Childs <rchilds@vctlabs.com>
2020-02-25 18:41:34 -0800

    Add python packaging infrastructure
    This is enough to successfully build an empty package:
      python3 ./setup.py sdist bdist_wheel

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+setuptools.setup(
+    name="conservator-cli",
+    version="0.0.1",
+    author="FLIR",
+    author_email="someone@somewhere",
+    description="Command-line tools using the FLIR Conservator API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FLIR/conservator-cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
