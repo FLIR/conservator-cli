b2047252141a327cc6d126f83501e18afe8aa06b
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-20 13:06:53 -0700

    style

--- FLIR/conservator_cli/app/upload/push_project.py
+++ FLIR/conservator_cli/app/upload/push_project.py
@@ -1,63 +1,51 @@
 #!/usr/bin/env python3
-import click
 import json
 import os
 import subprocess
 
+import click
+
 from FLIR.conservator_cli.lib import graphql_api as fca
 
-
-def find_folders(project_root, conservator_token):
+def upload_collection(folder_root, conservator_path, api_key, include_associated_files=False):
     folder_paths = []
     parent_ids = {}
-    for root, dirs, files in os.walk(project_root):
+    for root, dirs, files in os.walk(folder_root):
         path = root.split(os.sep)
         basename = os.path.basename(root)
         if "index.json" in files:
             dirs.clear()
             continue
-        collection = fca.get_collection_by_path("/"+root, conservator_token)
-
-        if not collection:
-            collection = fca.create_collection(basename, parent_ids[os.path.dirname(root)], conservator_token)
+        collection = fca.get_collection_by_path(os.path.join(conservator_root, root), api_key)
+        collection = collection or fca.create_collection(basename, parent_ids[os.path.dirname(root)], api_key)
         parent_ids[root] = collection["id"]
-        for file in files:
-            if file in ["wordcloud.png"]:
-                continue
-            if file.endswith(".png"):
-                application_type = "image/png"
-            elif file.endswith(".json"):
-                application_type = "application/json"
-            elif file.endswith(".csv"):
-                application_type = "text/csv"
-            else:
-                continue
-            data = fca.get_signed_collection_locker_url(collection["id"], application_type, file, conservator_token)
-            upload_results = fca.upload_video_to_s3(os.path.join(root, file), data["signedUrl"], application_type)
-            print(upload_results)
+        if include_associated_files:
+            for filename in files:
+                if filename in ["wordcloud.png"]:
+                    continue
+                if filename.endswith(".png"):
+                    application_type = "image/png"
+                elif filename.endswith(".json"):
+                    application_type = "application/json"
+                elif filename.endswith(".csv"):
+                    application_type = "text/csv"
+                else:
+                    continue
+                data = fca.get_signed_collection_locker_url(collection["id"], application_type, filename, api_key)
+                upload_results = fca.upload_video_to_s3(os.path.join(root, filename), data["signedUrl"], application_type)
+                print(upload_results)
         print(root)
         print(collection)
     return folder_paths
 
-def find_dataset_folders(project_root, rename_map={}):
-    folder_paths = {}
-    for root, dirs, files in os.walk(project_root):
-        path = root.split(os.sep)
-        basename = os.path.basename(root)
-        if "index.json" in files:
-            print("Found dataset: {}".format(root))
-            dataset_name = os.path.basename(root)
-            dataset_name = rename_map.get(dataset_name, dataset_name)
-            folder_paths[dataset_name] = root
-    return folder_paths
-
 @click.command()
-@click.argument('collection_path')
-@click.option('-t', '--conservator_token', prompt="Conservator Token", help="Conservator API Token")
+@click.argument('folder_root')
+@click.option('-p', '--conservator_path', prompt="Path of folder on Conservator", help="Conservator path", default="/")
 @click.option('-a', '--include_associated_files', help="download associated files", is_flag=True)
-def download_project_main(collection_path, conservator_token, include_associated_files):
-    find_folders(collection_path, conservator_token)
-
+@click.option('-k', '--api_key', prompt="Conservator Api Key", help="Conservator API Key")
+def upload_collection_main(folder_root, conservator_root, api_key, include_associated_files):
+    """This script recursively uploads FOLDER_ROOT to conservator at parent CONSERVATOR_PATH."""
+    upload_collection(folder_root, conservator_path, api_key, include_associated_files)
 
 if __name__ == "__main__":
-    download_project_main()
+    upload_collection_main()
