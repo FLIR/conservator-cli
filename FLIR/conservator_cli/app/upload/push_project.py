e6b2dd9f50a1b2a3a01fe549f44bd0cb96b7da3a
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-23 14:55:58 -0700

    abspath saves all

--- FLIR/conservator_cli/app/upload/push_project.py
+++ FLIR/conservator_cli/app/upload/push_project.py
@@ -17,16 +17,17 @@ def upload_collection(folder_root, root_collection_path, api_key, include_associ
         parent_ids[os.path.dirname(folder_root)] = conservator_root["id"]
     for root, dirs, files in os.walk(folder_root):
         relpath = os.path.relpath(root, start=os.path.dirname(folder_root))
+        parent_path = os.path.dirname(root)
         basename = os.path.basename(root)
         if "index.json" in files:
             dirs.clear()
             continue
-        collection_path = os.path.join(root_collection_path, relpath)
+        collection_path = os.path.abspath(os.path.join(root_collection_path, relpath))
         collection = fca.get_collection_by_path(str(collection_path), api_key)
-        if not collection and not os.path.dirname(root) in parent_ids:
+        if not collection and not parent_path in parent_ids:
             print("Could not find {} at root / in conservator.".format(relpath))
             exit()
-        collection = collection or fca.create_collection(basename, parent_ids[os.path.dirname(root)], api_key)
+        collection = collection or fca.create_collection(basename, parent_ids[parent_path], api_key)
         parent_ids[root] = collection["id"]
         if include_associated_files:
             for filename in files:
@@ -52,7 +53,7 @@ def upload_collection_main():
     parser.add_argument('-p', '--conservator_path', help="Conservator path", default="/")
     parser.add_argument('-k', '--api_key', help="Conservator API Key", required=True)
     args = parser.parse_args()
-    upload_collection(args.folder_root, args.conservator_path, args.api_key, args.include_associated_files)
+    upload_collection(os.path.abspath(args.folder_root), os.path.abspath(args.conservator_path), args.api_key, args.include_associated_files)
 
 if __name__ == "__main__":
     upload_collection_main()
