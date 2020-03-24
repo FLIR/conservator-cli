0f867685d973b63c5bb62b9dea0c86955dcc1dd8
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-23 17:55:15 -0700

    add tests to jenkins entry script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 DIR=$(realpath $(dirname ${BASH_SOURCE[0]}))
 nosetests -w $DIR/FLIR/conservator_cli/test/unit -v $@
+
+echo "---- STARTING FUNCTIONAL TESTS ----"
+
+echo "---- Running download_collection.sh test ----"
 $DIR/FLIR/conservator_cli/test/functional/download_collection.sh
+
+echo "---- Running index_tools_test.sh test ----"
+$DIR/FLIR/conservator_cli/test/functional/index_tools_test.sh
+
+echo "----  ENDING FUNCTIONAL TESTS  ----"
