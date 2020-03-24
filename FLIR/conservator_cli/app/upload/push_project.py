85070f19c457c0baeb1c6d89aee3ae70a60b3bf7
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-24 10:57:33 -0700

    break the uploading logic into a function, fix some documentation.

--- FLIR/conservator_cli/app/upload/push_project.py
+++ FLIR/conservator_cli/app/upload/push_project.py
@@ -6,6 +6,22 @@ import argparse
 
 from FLIR.conservator_cli.lib import graphql_api as fca
 
+def upload_associated_file(root, files, api_key):
+    for filename in files:
+        if filename in ["wordcloud.png"]:
+            continue
+        if filename.endswith(".png"):
+            application_type = "image/png"
+        elif filename.endswith(".json"):
+            application_type = "application/json"
+        elif filename.endswith(".csv"):
+            application_type = "text/csv"
+        else:
+            print("WARNING: File type of {} not supported, contributions accepted!".format(filename))
+            continue
+        data = fca.get_signed_collection_locker_url(collection["id"], application_type, filename, api_key)
+        upload_results = fca.upload_video_to_s3(os.path.join(root, filename), data["signedUrl"], application_type)
+
 def upload_collection(folder_root, root_collection_path, api_key, include_associated_files=False):
     folder_paths = []
     parent_ids = {}
@@ -30,27 +46,17 @@ def upload_collection(folder_root, root_collection_path, api_key, include_associ
         collection = collection or fca.create_collection(basename, parent_ids[parent_path], api_key)
         parent_ids[root] = collection["id"]
         if include_associated_files:
-            for filename in files:
-                if filename in ["wordcloud.png"]:
-                    continue
-                if filename.endswith(".png"):
-                    application_type = "image/png"
-                elif filename.endswith(".json"):
-                    application_type = "application/json"
-                elif filename.endswith(".csv"):
-                    application_type = "text/csv"
-                else:
-                    print("WARNING: File type of {} not supported, contributions accepted!".format(filename))
-                    continue
-                data = fca.get_signed_collection_locker_url(collection["id"], application_type, filename, api_key)
-                upload_results = fca.upload_video_to_s3(os.path.join(root, filename), data["signedUrl"], application_type)
+            upload_associated_files(root, files, api_key)
     return folder_paths
 
 def upload_collection_main():
-    """This script recursively uploads FOLDER_ROOT to conservator at parent CONSERVATOR_PATH. Folder is uploaded with the same name."""
+    """
+    This script recursively uploads FOLDER_ROOT to conservator at parent CONSERVATOR_PATH. Folder is uploaded with the same name.
+    See upload_associated_file for a list of supported file extensions.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('folder_root', help="path of folder to upload to conservator as a collection")
-    parser.add_argument('-a', '--include_associated_files', help="download associated files", action='store_true')
+    parser.add_argument('-a', '--include_associated_files', help="upload associated files", action='store_true')
     parser.add_argument('-p', '--conservator_path', help="Conservator path", default="/")
     parser.add_argument('-k', '--api_key', help="Conservator API Key", required=True)
     args = parser.parse_args()
