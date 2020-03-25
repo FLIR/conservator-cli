16a8db34c9721c9f870cbc2031e69832f3fa8480
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-24 22:12:32 -0700

    Place video metadata in its own folder, and associated files in a differnet one

--- FLIR/conservator_cli/test/functional/download_collection.sh
+++ FLIR/conservator_cli/test/functional/download_collection.sh
@@ -11,15 +11,15 @@ mkdir -p $TMP
 rm -rf $TMP/*
 
 cd $TMP
-$APP/download/download_collection.py /integration-test -u $CONSERVATOR_EMAIL -t $CONSERVATOR_TOKEN > $TMP/download_collection.log
+$APP/download/download_collection.py /integration-test -a -u $CONSERVATOR_EMAIL -t $CONSERVATOR_TOKEN > $TMP/download_collection.log
 assert_file_exists downloaded_collection $TMP/integration-test
 touch $TMP/integration-test/delete_me.txt
 $APP/download/download_collection.py /integration-test -u $CONSERVATOR_EMAIL -t $CONSERVATOR_TOKEN -o -a -d -m > $TMP/download_collection_all.log
 
 assert_file_exists downloaded_subfolder $TMP/integration-test/subfolder
-assert_file_exists downloaded_video_metadata $TMP/integration-test/5_second_cut.json
-assert_file_exists downloaded_associated_files_png $TMP/integration-test/car_arrow.png
-assert_file_exists downloaded_associated_files_json $TMP/integration-test/nntc_config.json
+assert_file_exists downloaded_video_metadata $TMP/integration-test/video_metadata/5_second_cut.json
+assert_file_exists downloaded_associated_files_png $TMP/integration-test/associated_files/car_arrow.png
+assert_file_exists downloaded_associated_files_json $TMP/integration-test/associated_files/nntc_config.json
 assert_file_exists downloaded_dataset_git $TMP/integration-test/test/index.json
 assert_file_exists downloaded_dataset_cvc integration-test/test/data/video-n22QqCcd2vRWZGK3z-frame-000225-AKP56dB2LuL5HY7hq.jpg
 assert_file_does_not_exist overwrite_file integration-test/delete_me.txt
