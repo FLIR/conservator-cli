452cecea85610377a4ea300750c8821bd7e8eabc
David Bernadett <david.bernadett@muncasterconsulting.com>
2020-03-25 13:02:48 -0700

    updating push_project to use associated_files and video_metadata folders

--- FLIR/conservator_cli/app/upload/push_project.py
+++ FLIR/conservator_cli/app/upload/push_project.py
@@ -6,21 +6,23 @@ import argparse
 
 from FLIR.conservator_cli.lib import graphql_api as fca
 
-def upload_associated_file(root, files, api_key):
-    for filename in files:
-        if filename in ["wordcloud.png"]:
-            continue
-        if filename.endswith(".png"):
-            application_type = "image/png"
-        elif filename.endswith(".json"):
-            application_type = "application/json"
-        elif filename.endswith(".csv"):
-            application_type = "text/csv"
-        else:
-            print("WARNING: File type of {} not supported, contributions accepted!".format(filename))
-            continue
-        data = fca.get_signed_collection_locker_url(collection["id"], application_type, filename, api_key)
-        upload_results = fca.upload_video_to_s3(os.path.join(root, filename), data["signedUrl"], application_type)
+def upload_associated_files(associated_file_folder, collection_id, api_key):
+    for root, dirs, associated_files in os.walk(os.path.join(associated_file_folder)):
+        for filename in associated_files:
+            if filename in ["wordcloud.png"]:
+                continue
+            if filename.endswith(".png"):
+                application_type = "image/png"
+            elif filename.endswith(".json"):
+                application_type = "application/json"
+            elif filename.endswith(".csv"):
+                application_type = "text/csv"
+            else:
+                print("WARNING: File type of {} not supported, contributions accepted!".format(filename))
+                continue
+            data = fca.get_signed_collection_locker_url(collection_id, application_type, filename, api_key)
+            upload_results = fca.upload_video_to_s3(os.path.join(root, filename), data["signedUrl"], application_type)
+        break
 
 def upload_collection(folder_root, root_collection_path, api_key, include_associated_files=False):
     folder_paths = []
@@ -38,6 +40,8 @@ def upload_collection(folder_root, root_collection_path, api_key, include_associ
         if "index.json" in files:
             dirs.clear()
             continue
+        if "video_metadata" in dirs:
+            dirs.remove("video_metadata")
         collection_path = os.path.abspath(os.path.join(root_collection_path, relpath))
         collection = fca.get_collection_by_path(str(collection_path), api_key)
         if not collection and not parent_path in parent_ids:
@@ -45,8 +49,10 @@ def upload_collection(folder_root, root_collection_path, api_key, include_associ
             exit()
         collection = collection or fca.create_collection(basename, parent_ids[parent_path], api_key)
         parent_ids[root] = collection["id"]
-        if include_associated_files:
-            upload_associated_files(root, files, api_key)
+
+        if "associated_files" in dirs and include_associated_files:
+            upload_associated_files(os.path.join(root, "associated_files"), collection["id"], api_key)
+            dirs.remove("associated_files")
     return folder_paths
 
 def upload_collection_main():
