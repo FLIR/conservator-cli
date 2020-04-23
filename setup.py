1c65d656c27c2cd8b822d90b54a832947b8e7401
Ron Lockwood-Childs <rchilds@vctlabs.com>
2020-04-23 13:27:39 -0700

    Bump version for ConservatorTools requirements.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="conservator-cli",
-    version="0.0.1",
+    version="0.0.2",
     author="FLIR",
     author_email="someone@somewhere",
     description="Command-line tools using the FLIR Conservator API",
