72669760834b4df217cc347f30d1e5edc2550a32
Ron Lockwood-Childs <rchilds@vctlabs.com>
2020-04-23 14:02:49 -0700

    Bumped setup.py version, fixed package installation
    The project wasn't installing properly without supplying the `-e` option
    to pip.  This change specifically calls out which directories are to be
    installed so that the namespace package can be found on imports
    (otherwise, the import fails with "unknown module FLIR").

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/FLIR/conservator-cli",
-    packages=setuptools.find_packages(),
+    packages=["FLIR.conservator_cli.lib", "FLIR.conservator_cli.app"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
