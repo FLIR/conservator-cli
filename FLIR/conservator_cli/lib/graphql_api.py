c3052b109453ad68c7fa498151191e3f51fe93dd
Justin Dong <jsdong@ucsb.edu>
2020-05-15 14:05:01 -0700

    added refactored wordcloud code

--- FLIR/conservator_cli/lib/graphql_api.py
+++ FLIR/conservator_cli/lib/graphql_api.py
@@ -224,6 +224,21 @@ def get_collection_by_id(id, access_token):
 	}
 	return query_conservator(query, variables, access_token)["collection"]
 
+def get_videos_by_collectionID(collectionID, access_token):
+	query = """
+	query videos($collectionId: ID!) {
+		videos(collectionId: $collectionId, limit:100000) {
+			id 
+			filename
+			url
+			tags
+		} 
+	}
+	"""
+	variables = {
+		"collectionId": collectionID
+	}
+	return query_conservator(query, variables, access_token)
 
 def get_datasets_from_collection(collection_id, access_token):
 	query = """
