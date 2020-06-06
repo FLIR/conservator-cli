c25686793402ae3cef29d514148bbe93422b0870
Ron Lockwood-Childs <rchilds@vctlabs.com>
2020-06-05 17:59:38 -0700

    Pull in a couple modules to avoid external dependencies

---
+++ FLIR/conservator_cli/lib/execute_command.py
@@ -0,0 +1,19 @@
+import blessed
+import os
+import subprocess
+
+t = blessed.Terminal()
+
+def execute_command(args=[], silent=False, cwd=None, abort_on_failure=False, env=None):
+
+    # Run command
+    cmd_str = " ".join(args)
+    print(t.blue(cmd_str))
+    stdout = stderr = [None,subprocess.PIPE][silent]
+    status = subprocess.run(args, cwd=cwd, stdout=stdout, stderr=stderr, env=env).returncode
+
+    # Report possible error
+    if status != 0 and abort_on_failure:
+        raise Exception("execute_command failed: {}".format(" ".join(cmd_str)))
+
+    return status
