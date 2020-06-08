36657d15abd51fdc0fcc12a513a472856d9f9885
Ron Lockwood-Childs <rchilds@vctlabs.com>
2020-06-08 14:56:26 -0700

    Add flush to terminal progress bar
    When piping output (E.G. to the `tee` command), or mixing stdout and
    stderr output (when using logging), it makes more sense to flush the
    progress bar output, instead of buffering in those cases which defeats
    the purpose of the progress bar, to show that something is happening..

--- FLIR/conservator_cli/lib/terminal_progress_bar.py
+++ FLIR/conservator_cli/lib/terminal_progress_bar.py
@@ -18,7 +18,7 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r', flush=True)
 
 #Wraps a File so we can print the progress as requests uploads the file
 class ProgressFileWrapper:
