cbe0e001f86f5755060894ab1dd5e62a863046cc
Patryk Seweryn <patryk.seweryn@enigmapattern.com>
2020-06-17 16:26:27 +0200

    added a few additional functions.

--- FLIR/conservator_cli/lib/hierarchy_tree/wn_helpers.py
+++ FLIR/conservator_cli/lib/hierarchy_tree/wn_helpers.py
@@ -2,13 +2,18 @@ import nltk
 import re
 from typing import Union
 
+from networkx import DiGraph
 from nltk.corpus import wordnet as wn
+from nltk.corpus import wordnet_ic
 from nltk.corpus.reader import Synset
+from nltk.wsd import lesk
 
 nltk.download('wordnet')
+nltk.download('wordnet_ic')
 
+brown_ic = wordnet_ic.ic('ic-brown.dat')
 
-def get_name(synset_name: Union[Synset, str]):
+def get_name(synset_name: Union[Synset, str]) -> str:
     if type(synset_name) is Synset:
         name = synset_name.name()
     else:
@@ -16,19 +21,21 @@ def get_name(synset_name: Union[Synset, str]):
     return re.search(r'([\w._\"\'-]+)\.\w\.\d{2}$', name).group(1)
 
 
-def check_if_noun(synset):
+def check_if_noun(synset: Synset) -> bool:
     return synset.pos() == wn.NOUN
 
 
-def contains_physical_object(hierarchy):
-    return any(['physical_entity' in get_name(i) for i in hierarchy])
+def contains_physical_object(hierarchy_of_synsets: list) -> bool:
+    return any(['physical_entity' in get_name(i) for i in hierarchy_of_synsets])
 
 
-def is_last_word_same_as_the_key(key: str, hierarchy: list):
+def is_last_word_same_as_the_key(key: str, hierarchy: list) -> bool:
     return key == get_name(hierarchy[-1])
+
 
 def get_synsets(word: str) -> list:
     return wn.synsets(word, pos=wn.NOUN)
+
 
 def get_hypernym_paths(word: str) -> list:
     synsets = wn.synsets(word, pos=wn.NOUN)
@@ -41,7 +48,7 @@ def get_hypernym_paths(word: str) -> list:
         return hypernym_paths
 
 
-def _get_entity_if_search_class_matched(search_class, item):
+def _get_entity_if_search_class_matched(search_class: str, item: str):
     match = re.match(re.escape(search_class) + r'\.\w\.\d{2}$', item)
     return match.group() if match else None
 
@@ -51,5 +58,49 @@ def get_entities_containing_search_class(search_class: str, list_of_entities: li
     return list(filter(lambda match: match is not None, list_of_matches))
 
 
-def get_definition(entity_name: str):
+def get_definition(entity_name: str) -> str:
     return wn.synset(entity_name).definition()
+
+def get_synset_by_name(name: str):
+    return wn.synset(name)
+
+
+def get_most_similar_nodes_in_graph(graph: DiGraph, name: Synset) -> list:
+    similarities = {}
+    for node in graph.nodes():
+        similarities[node] = wn.lin_similarity(name, wn.synset(node), ic=brown_ic)
+        # similarities[node] = wn.wup_similarity(name, wn.synset(node))
+    return sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+
+def get_best_synset(word: str, context_sentence: str) -> Synset:
+    return lesk(context_sentence.split(), word, pos=wn.NOUN, synsets=wn.synsets(word))
+
+def get_synsets_with_definition(word):
+    return {
+        s.name(): s.definition() for s in get_synsets(word)
+    }
+
+
+if __name__ == '__main__':
+    words = ['computer', 'mouse', 'car']
+    for word in words:
+        synsets = get_synsets(word)
+        print(f'Word we are trying to find a proper meaning: {word}')
+        for s in synsets:
+            print(f'found word: {s.name()}, definition: {s.definition()}')
+
+
+
+
+
+
+
+
+
+    word = 'mouse'
+    context_sentence = 'An electronic device used together with computer'
+
+    best_synset = get_best_synset(word, context_sentence)
+    print(f'{best_synset.name()}, {best_synset.definition()}')
+
+    print(f'end.')
