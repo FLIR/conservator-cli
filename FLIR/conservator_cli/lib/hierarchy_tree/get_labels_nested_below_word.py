7d54d92872371cf54fdab328959a6f1e244443e1
Patryk Seweryn <patryk.seweryn@enigmapattern.com>
2020-06-18 11:03:12 +0200

    created command line interface to run get_labels_nested_below_word.py

--- FLIR/conservator_cli/lib/hierarchy_tree/get_labels_nested_below_word.py
+++ FLIR/conservator_cli/lib/hierarchy_tree/get_labels_nested_below_word.py
@@ -1,13 +1,12 @@
+import argparse
 import logging
 from pathlib import Path
-from pprint import pprint
 
 from FLIR.conservator_cli.lib.hierarchy_tree.constants import DATA_PATH
 from FLIR.conservator_cli.lib.hierarchy_tree.graphs import get_successors_for_node, read_graph_from_file
-from FLIR.conservator_cli.lib.hierarchy_tree.utils import read_json_file, \
-    get_unique_classes_from_files
+from FLIR.conservator_cli.lib.hierarchy_tree.utils import read_json_file
 from FLIR.conservator_cli.lib.hierarchy_tree.wn_helpers import get_name, \
-    get_entities_containing_search_class, get_synsets, get_definition, get_most_similar_nodes_in_graph
+    get_entities_containing_search_class, get_definition
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s]: %(message)s',
@@ -17,6 +16,7 @@ lg = logging.getLogger(__name__)
 lg.setLevel(logging.INFO)
 
 
+# todo deprecated
 def form_dictionary_tree(list_of_hypernyms: list) -> dict:
     tree = {
         'id': 'entity.n.01',
@@ -45,35 +45,42 @@ def form_dictionary_tree(list_of_hypernyms: list) -> dict:
 
 
 def get_labels_nested_below_word():
-    graph_path = Path(DATA_PATH) / 'graph.yaml'
-    graph_info_path = Path(DATA_PATH) / 'graph_info.json'
+    parser = argparse.ArgumentParser(description='get all labels similar to a word based on a graph.')
+    parser.add_argument('-p', '--path_to_graph', type=str, help='path where graph is saved', required=True)
+    parser.add_argument('-w', '--word', type=str, required=True)
 
-    graph = read_graph_from_file(graph_path)
+    args = parser.parse_args()
+    path_to_graph: str = args.path_to_graph
+    provided_word: str = args.word
+
+    yaml_graph_path = Path(path_to_graph) / 'graph.yaml'
+    graph_info_path = Path(path_to_graph) / 'graph_info.json'
+
+    graph = read_graph_from_file(yaml_graph_path)
     graph_info = read_json_file(graph_info_path)
 
-    search_class = 'wheeled_vehicle'
-    found_targets = get_entities_containing_search_class(search_class, graph.nodes())
+    found_targets = get_entities_containing_search_class(provided_word, graph.nodes())
 
     if len(found_targets) == 0:
-        raise IndexError(f'there are no found synsets for specific phrase: {search_class}')
+        raise IndexError(f'there are no found synsets for specific phrase: {provided_word}')
     else:
+        lg.info(f'Found nodes for provided word ({provided_word}): ')
         for i, target in enumerate(found_targets):
             lg.info(f'{i}: {target}')
+    lg.info('*' * 40)
 
     searched_synset = found_targets[0]
 
     list_with_items = get_successors_for_node(graph, searched_synset)
-
+    lg.info(f'Successors for {searched_synset} :')
     for item in list_with_items:
-        lg.info(item, get_definition(item))
+        lg.info(f'{item}, {get_definition(item)}')
     names = [get_name(s) for s in list_with_items]
     names = [name.replace('_', ' ') for name in names]
 
     commands = [f'hasPrediction:"{name}"' for name in names]
-
-    lg.info(f'search command for Conservator : ')
-    search_text = f'{" OR ".join(commands)}'
-    lg.info(search_text)
+    lg.info('*' * 40)
+    lg.info(f'search command for Conservator : {" OR ".join(commands)}')
 
 
 if __name__ == '__main__':
