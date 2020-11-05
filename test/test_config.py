762a62bd8320c4279a80efde9b28052d43675d59
n-wach <nmwachholz@gmail.com>
2020-11-05 14:20:19 -0800

    Add pytest and config tests

---
+++ test/test_config.py
@@ -0,0 +1,34 @@
+import os
+from conservator.config import Config
+
+TEST_DICT = {
+    Config.EMAIL: "test@example.com",
+    Config.API_KEY: "testAPIkey",
+    Config.URL: "https://myconservator.com",
+}
+
+
+def test_from_dict():
+    c = Config.from_dict(TEST_DICT)
+
+    assert c.email == "test@example.com"
+    assert c.key == "testAPIkey"
+    assert c.url == "https://myconservator.com"
+
+
+def test_save_and_load_file():
+    c1 = Config.from_dict(TEST_DICT)
+    c1.save_to_file("/tmp/test.config")
+    c2 = Config.from_file("/tmp/test.config")
+    assert c1 == c2
+    assert c2.email == "test@example.com"
+    assert c2.key == "testAPIkey"
+    assert c2.url == "https://myconservator.com"
+
+
+def test_from_environ():
+    os.environ.update(TEST_DICT)
+    c = Config.from_environ()
+    assert c.email == "test@example.com"
+    assert c.key == "testAPIkey"
+    assert c.url == "https://myconservator.com"
