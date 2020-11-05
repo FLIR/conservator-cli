15ed6d4db7be8e8ff24da8948ecfae2f843d7f23
n-wach <nmwachholz@gmail.com>
2020-11-05 14:24:06 -0800

    Add config CLI command

--- conservator/cli/__init__.py
+++ conservator/cli/__init__.py
@@ -1,7 +1,21 @@
 import click
+from conservator.config import Config
 
 
 @click.group()
 def main():
     # TODO: logging verbosity
     pass
+
+
+@main.command(help="View or delete the current config")
+@click.option('--delete', is_flag=True, help="If specified, delete the default credentials.")
+def config(delete):
+    if delete:
+        Config.delete_saved_default_config()
+        return
+    default_config = Config.default()
+    click.echo("Default config:")
+    click.echo(f"  Email: {default_config.email}")
+    click.echo(f"  Key: {default_config.key}")
+    click.echo(f"  URL: {default_config.url}")
