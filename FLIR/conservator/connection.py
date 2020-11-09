814e2fd3e09ee64eeb85a5235603116609a0bddf
n-wach <nmwachholz@gmail.com>
2020-11-09 11:46:33 -0800

    Update docs

--- FLIR/conservator/connection.py
+++ FLIR/conservator/connection.py
@@ -105,27 +105,46 @@ class ConservatorConnection:
 
         max_depth = max(map(lambda f: f.count("."), include_fields + exclude_fields)) + 1
 
-        # Fields are included in a query by calling them.
-        # View the SGQLC docs for more info.
-        def recursive_add_fields(obj, path="", depth=0):
-            field_names = [name for name in dir(obj) if not name.startswith("_")]
-            if len(field_names) == 0 or depth >= max_depth:
-                obj()
-                return
-
-            for field_name in field_names:
-                field = getattr(obj, field_name)
-                prefix = ("" if path == "" else path + ".")
-                cur_path = prefix + field_name
-                # A field is included if its path is in include_fields
-                # or it is more specific than something in include_fields.
-                for included in include_fields:
-                    # Unless it is excluded.
-                    if (cur_path.startswith(included) or included.startswith(cur_path))\
-                            and cur_path not in exclude_fields:
-                        recursive_add_fields(field, cur_path, depth + 1)
-                        break
-
-        recursive_add_fields(query)
+        ConservatorConnection.recursive_add_fields(query, include_fields, exclude_fields, max_depth=max_depth)
 
         return getattr(self.run(op), query_name)
+
+    @staticmethod
+    def recursive_add_fields(obj, include_fields=(), exclude_fields=(), path="", depth=0, max_depth=2):
+        """
+        Adds fields to an SGQLC operation.
+
+        :param obj: The SGQLC object to add fields to.
+        :param include_fields: List of field paths to include in the returned object(s).
+            Fields of subtypes can be specified as a path separated by ``.``.  For instance,
+            when querying for a Dataset, you could add ``repository.master`` to only
+            include the ``master`` field of ``Dataset.repository``.
+            If empty or not specified, all fields will be included.
+        :param exclude_fields: A list of field paths to exclude. These take the same form as
+            included fields, and override them. In the above example, excluding ``repository``
+            will exclude all fields of ``repository``.
+        :param path: The current path, used to filter included and excluded path fields.
+        :param depth: How many times this has been called recursively.
+        :param max_depth: The maximum depth of fields to add.
+        """
+        field_names = [name for name in dir(obj) if not name.startswith("_")]
+        if len(field_names) == 0 or depth >= max_depth:
+            # Fields are included in a query by calling them.
+            # View the SGQLC docs for more info.
+            obj()
+            return
+
+        for field_name in field_names:
+            field = getattr(obj, field_name)
+            prefix = ("" if path == "" else path + ".")
+            cur_path = prefix + field_name
+            # A field is included if its path is in include_fields
+            # or it is more specific than something in include_fields.
+            for included in include_fields:
+                # Unless it is excluded.
+                if (cur_path.startswith(included) or included.startswith(cur_path)) \
+                        and cur_path not in exclude_fields:
+                    ConservatorConnection.recursive_add_fields(field, include_fields,
+                                                               exclude_fields, cur_path,
+                                                               depth + 1, max_depth)
+                    break
--- docs/api.rst
+++ docs/api.rst
@@ -7,4 +7,18 @@ Config
 .. automodule:: FLIR.conservator.config
     :members:
 
+Conservator Connection
+----------------------
+
+.. automodule:: FLIR.conservator.connection
+
+    .. automethod:: FLIR.conservator.connection.ConservatorConnection.query(query, operation_base=Query, include_fields=(), exclude_fields=(), **kwargs)
+
+    :members:
+
+Conservator
+-----------
+
+.. automodule:: FLIR.conservator.conservator
+    :members:
 
