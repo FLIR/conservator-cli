23bddce72a0ec753b026d1e72dabcff7ece672f0
n-wach <nmwachholz@gmail.com>
2020-11-13 18:41:09 -0800

    Add documentation and API Quickstart

--- FLIR/conservator/__init__.py
+++ FLIR/conservator/__init__.py
@@ -1,2 +1,3 @@
 if __name__ == "__main__":
+    import FLIR.conservator.cli as cli
     cli.main()
--- FLIR/conservator/fields_request.py
+++ FLIR/conservator/fields_request.py
@@ -1,6 +1,23 @@
 class FieldsRequest:
     """
-    A collection of fields to include (or exclude) in a query.
+    A collection of fields to include (or exclude) in a query. Many different
+    API calls will require specifying a list of fields using a :class:`FieldsRequest`.
+
+    It can be used with :class:`FLIR.conservator.connection.Connection.query`::
+        >>> conservator = Conservator.default()
+        >>> fields = FieldsRequest()
+        >>> fields.include_field("id", "name")
+        >>> datasets = conservator.query(Query.datasets, fields=fields, search_text="ADAS")
+        >>> for dataset in datasets:
+        ...     print(dataset.name)
+
+    As used in this module, a `field_path` can refer to either:
+        - A single field, like `"name"` to select `Project.name`
+        - An entire subfield, like `"repository"` select all fields in `Dataset.reposistory`
+        - A path to a field/subfield in a subfield, like `"repository.master"`
+          to select the `Reposistory.master` field in `Dataset.reposistory`
+
+    These paths can be as long/specific as you want.
 
     :param include_fields: List of field paths to include in the returned object(s).
         Fields of subtypes can be specified as a path separated by ``.``.  For instance,
@@ -55,6 +72,7 @@ class FieldsRequest:
                 self.add_fields_to_request(field, field_path, current_depth + 1)
 
     def should_include_path(self, path):
+        """Returns `True` if this request should include `path`."""
         # if excluded, definitely no
         if path in self.excluded:
             return False
@@ -66,16 +84,20 @@ class FieldsRequest:
         return False
 
     def include_field(self, *field_path):
+        """Includes `field_path` in the request."""
         self.include_fields(field_path)
 
     def exclude_field(self, *field_path):
+        """Excludes `field_path` from the request."""
         self.exclude_fields(field_path)
 
     def include_fields(self, field_paths):
+        """Includes `field_paths` in the request."""
         if "" in self.included:
             self.included.remove("")
         self.included = self.included.union(field_paths)
 
     def exclude_fields(self, field_paths):
+        """Excludes `field_paths` from the request."""
         self.excluded = self.excluded.union(field_paths)
 
