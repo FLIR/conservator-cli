99bc32b9f740683fdea8c60c0430c075b5a464c0
Nathan Wachholz <nmwachholz@gmail.com>
2020-12-10 19:28:06 -0800

    Allow unpacking a specific field in a PaginatedQuery (#86)
    * Allow PaginatedQuery to unpack a field
    
    * Add fields to example
    
    * Add documentation for PaginatedQuery

---
+++ examples/list_dataset_frames.py
@@ -0,0 +1,17 @@
+from FLIR.conservator.conservator import Conservator
+from FLIR.conservator.fields_request import FieldsRequest
+from FLIR.conservator.paginated_query import PaginatedQuery
+from FLIR.conservator.generated.schema import Query, DatasetFrame
+
+conservator = Conservator.default()
+fields = FieldsRequest()
+fields.include_field("dataset_frames.frame_id")
+fields.include_field("dataset_frames.width")
+fields.include_field("dataset_frames.height")
+query_results = PaginatedQuery(conservator, query=Query.dataset_frames_only,
+                               unpack_field="dataset_frames", fields=fields,
+                               id="RkAXSN4ychHgiNkMk")
+
+for frame in query_results:
+    print(frame)
+
