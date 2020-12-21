8a92312da7c08e715125d41632f3254ab53f94aa
n-wach <nmwachholz@gmail.com>
2020-12-21 14:28:17 -0800

    Change example to use projects

--- examples/query_by_exact_name.py
+++ examples/query_by_exact_name.py
@@ -2,30 +2,30 @@ from FLIR.conservator.conservator import Conservator
 
 conservator = Conservator.default()
 
-video_name = "00965760_cam2.avi"
+project_name = "ADAS"
 
 # The "name" field is searched by default, but we need to add a filter
 # to make sure that the name is an exact match. Searches only check for
-# substring containment, so searching "my video" would match "my video"
-# and also something like "my video TEST".
+# substring containment, so searching "ADAS" would match "ADAS"
+# and also something like "ADAS External".
 
-# With filtered_by, we make sure to only return videos where the name is
+# With filtered_by, we make sure to only return projects where the name is
 # an exact match.
 print("Normal search:")
-results = conservator.videos.search(video_name, fields="name").filtered_by(
-    name=video_name
+results = conservator.projects.search(project_name, fields="name").filtered_by(
+    name=project_name
 )
-for video in results:
-    assert video.name == video_name
-    print(video)
+for project in results:
+    assert project.name == project_name
+    print(project)
 
 print()
 print("Search by name field only:")
 # We could also slightly speed up the query using Conservator's Advanced
 # Search syntax. We can specify which field to search in the search text:
-results = conservator.videos.search(f'name:"{video_name}"', fields="name").filtered_by(
-    name=video_name
+results = conservator.projects.search(f'name:"{project_name}"', fields="name").filtered_by(
+    name=project_name
 )
-for video in results:
-    assert video.name == video_name
-    print(video)
+for project in results:
+    assert project.name == project_name
+    print(project)
