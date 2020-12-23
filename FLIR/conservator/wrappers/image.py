5f96364c56b176c57080f33223ef1491a953be39
Nathan Wachholz <nmwachholz@gmail.com>
2020-12-23 15:35:53 -0800

    Remove get_frames--use frames field instead (#116)

--- FLIR/conservator/wrappers/image.py
+++ FLIR/conservator/wrappers/image.py
@@ -28,17 +28,3 @@ class Image(QueryableType):
     def download(self, path):
         """Download image to ``path``."""
         download_file(path, self.filename, self.url)
-
-    def get_frames(self, index, start_index, custom_metadata, fields=None):
-        """
-        Returns the image's frames, with the specified `fields`.
-        """
-        return self._conservator.query(
-            schema.Image.frames,
-            operation_base=schema.Image,
-            fields=fields,
-            id=self.id,
-            frame_index=index,
-            start_frame_index=start_index,
-            custom_metadata=custom_metadata,
-        )
