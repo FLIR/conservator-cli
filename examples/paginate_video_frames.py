44ae0d3b4cd713fb4725dc2e16c5736fc15ddc61
Nathan Wachholz <nmwachholz@gmail.com>
2021-01-01 14:53:03 -0800

    Paginate video frames (#128)
    * Update Video.get_frames
    
    * Reformat
    
    * Add pagination example
    
    * Wrap returned Frames
    
    * Add pagination generator to library

---
+++ examples/paginate_video_frames.py
@@ -0,0 +1,20 @@
+from FLIR.conservator.conservator import Conservator
+from FLIR.conservator.fields_request import FieldsRequest
+
+conservator = Conservator.default()
+fields = FieldsRequest()
+fields.include_field("video_id")
+fields.include_field("width")
+fields.include_field("height")
+fields.include_field("annotations.bounding_box")
+fields.include_field("annotations.labels")
+
+video = conservator.videos.all().including("name", "frames_count").first()
+print(video)
+
+# Unless you're working with huge videos (10k+ frames), you'll probably
+# find it easier and faster to just populate all frames at once. Only
+# use this method if your request for all frames is timing out, or too large
+# to handle at once.
+for frame in video.get_all_frames_paginated(fields):
+    print(frame)
