99d7d2a515bd123d1107ff0168ee1ad5945bd1ca
Nathan Wachholz <nmwachholz@gmail.com>
2021-01-05 16:07:08 -0800

    Use tmp directory for running tests (#133)

--- test/test_local_dataset.py
+++ test/test_local_dataset.py
@@ -11,6 +11,7 @@ def test_clone():
     conservator = Conservator.default()
     dataset = conservator.datasets.from_id(DATASET_ID)
     cloned_dataset = LocalDataset.clone(dataset)
+    assert cloned_dataset is not None
     assert os.path.abspath(DATASET_PATH) == cloned_dataset.path
     assert os.path.exists(DATASET_PATH)
     assert os.path.exists(DATASET_PATH + "/index.json")
