ed296bbc4fc67709fd8b5536d157ffa08cd04567
n-wach <nmwachholz@gmail.com>
2021-01-11 14:51:05 -0800

    Add python API to find/download blob by name

--- FLIR/conservator/wrappers/dataset.py
+++ FLIR/conservator/wrappers/dataset.py
@@ -145,6 +145,16 @@ class Dataset(QueryableType):
             commit_id=commit_id,
         )
 
+    def get_root_tree_id(self, commit_id="HEAD"):
+        """
+        Returns the id (hash) of the tree at the given `commit_id`. The ID
+        can be a hash, or an identifier like ``HEAD``.
+
+        Defaults to the latest commit.
+        """
+        commit = self.get_commit_by_id(commit_id=commit_id, fields="tree")
+        return commit.tree
+
     def get_tree_by_id(self, tree_id="HEAD", fields=None):
         """
         Returns a tree from a `tree_id`. The ID can be a hash, or an
@@ -161,16 +171,52 @@ class Dataset(QueryableType):
         """
         return f"{self.get_git_url()}/get_blob/{blob_id}"
 
+    def get_blob_id_by_name(self, filename, commit_id="HEAD"):
+        """
+        Returns a blob's id (hash) from `filename`. This searches the root directory of
+        the given `commit_id`, and then searches `associated_files`. It returns the hash of
+        the first blob found with a matching name.
+        """
+        fields = ["tree_list.name", "tree_list._id", "tree_list.type"]
+        root_tree_id = self.get_root_tree_id(commit_id)
+        root_tree = self.get_tree_by_id(tree_id=root_tree_id, fields=fields)
+        associated_files_tree_id = None
+        for item in root_tree.tree_list:
+            if item.name == filename and item.type == "blob":
+                return item._id
+            if item.name == "associated_files" and item.type == "tree":
+                associated_files_tree_id = item._id
+
+        associated_files_tree = self.get_tree_by_id(tree_id=associated_files_tree_id, fields=fields)
+        for item in associated_files_tree.tree_list:
+            if item.name == filename and item.type == "blob":
+                return item._id
+
+    def download_blob_by_name(self, filename, path, commit_id="HEAD"):
+        """
+        Download a blob to the specified `path`. A `blob_id` can be
+        gotten using :meth:`~FLIR.conservator.wrappers.dataset.Dataset.get_tree_by_id`.
+
+        If `path` is a file, the blob will be saved to that file. If it is a directory,
+        the blob will be saved to a file named `filename` within the directory at `path`.
+        """
+        path = os.path.abspath(path)
+        if os.path.isdir(path):
+            path = os.path.join(path, filename)
+
+        blob_id = self.get_blob_id_by_name(filename, commit_id=commit_id)
+        self.download_blob(blob_id, path)
+
     def download_blob(self, blob_id, path):
         """
         Download a blob to the specified `path`. A `blob_id` can be
         gotten using :meth:`~FLIR.conservator.wrappers.dataset.Dataset.get_tree_by_id`.
         """
-        url = self.get_blob_url_by_id(blob_id)
         path = os.path.abspath(path)
         if os.path.exists(path):
             raise FileExistsError("Blob download path must not exist")
 
+        url = self.get_blob_url_by_id(blob_id)
         parent, name = os.path.split(path)
         os.makedirs(parent, exist_ok=True)
         download_file(parent, name, url)
@@ -184,16 +230,7 @@ class Dataset(QueryableType):
         This can be used as a faster alternative to a full repository
         clone for some operations.
         """
-        path = os.path.abspath(path)
-        if os.path.isdir(path):
-            path = os.path.join(path, "index.json")
-
-        fields = ["tree_list.name", "tree_list._id"]
-        latest_tree = self.get_tree_by_id(tree_id="HEAD", fields=fields)
-        for item in latest_tree.tree_list:
-            if item.name == "index.json":
-                self.download_blob(item._id, path)
-                return
+        self.download_blob_by_name("index.json", path, commit_id="HEAD")
 
     @classmethod
     def from_local_path(cls, conservator, path="."):
