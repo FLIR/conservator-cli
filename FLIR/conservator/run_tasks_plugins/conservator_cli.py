a33ecca0ce7b6a2e86f76e4d5a3f6815c93816d5
Ron Lockwood-Childs <rchilds@vctlabs.com>
2021-02-16 01:46:08 -0800

    conservator_cli plugin: fix args to plugin
    * only use one level of command name instead of two,
      since not all conservator tool commands take a subcommand
      (most of commands do however, so the first element in "args"
      will be the subcommand, see task_config in example configs)
    
    * remember to pass in log argument

--- FLIR/conservator/run_tasks_plugins/conservator_cli.py
+++ FLIR/conservator/run_tasks_plugins/conservator_cli.py
@@ -1,8 +1,9 @@
 from FLIR.conservator.cli import main
 
 
-def conservator_cli(cmd_class, cmd_name, args=(), log="INFO", task=None):
-    cli_cmd = [cmd_class, cmd_name] + list(args)
+def conservator_cli(cmd, args=(), log="INFO", task=None):
+    log_arg = "--log=" + log
+    cli_cmd = [log_arg, cmd] + list(args)
     result = main(cli_cmd, standalone_mode=False)
     print("RESULT:", result)
     return result
