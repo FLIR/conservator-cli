98eb333c56f40ad36d25d099b6cf31c4dff723c4
Nathan Wachholz <nmwachholz@gmail.com>
2021-03-01 11:18:54 -0800

    Stop excluding and retrying queries with problematic fields (#201)
    * Remove problematic field logic
    
    * Remove graphql_to_python(name)

--- FLIR/conservator/fields_manager.py
+++ FLIR/conservator/fields_manager.py
@@ -1,31 +1,11 @@
-import re
-import keyword
-
 import sgqlc.types
 from FLIR.conservator.generated import schema
 
 
 class FieldsManager:
     """
-    Stores a map of problematic fields for each SGQLC type. These
-    are used during queries to avoid errors.
+    Defines default fields for each SGQLC type.
     """
-
-    def __init__(self):
-        self.problematic_fields = {
-            schema.Repository: {"id"},
-            schema.Dataset: {"repository.id"},
-        }
-
-    def get_problematic_paths(self, typ):
-        """Get problematic field paths for a given SGQLC `type`"""
-        return self.problematic_fields.get(typ, ())
-
-    def add_problematic_path(self, typ, path):
-        """Add a problematic field path for a given SGQLC `type`"""
-        if self.problematic_fields.get(typ) is None:
-            self.problematic_fields[typ] = set()
-        self.problematic_fields[typ].add(path)
 
     @classmethod
     def select_default_fields(cls, selector):
@@ -218,19 +198,3 @@ class FieldsManager:
             selector.flagged_frames()
             selector.full_res_mp4_url()
             selector.full_res_mp4_status()
-
-    re_camel_case_words = re.compile("([^A-Z]+|[A-Z]+[^A-Z]*)")
-
-    @staticmethod
-    def graphql_to_python(name):
-        """
-        Converts a GraphQL name to python in the same way as SGQLC codegen.
-        """
-        # This code copied from SGQLC codegen
-        s = []
-        for w in FieldsManager.re_camel_case_words.findall(name):
-            s.append(w.lower())
-        name = "_".join(s)
-        if keyword.iskeyword(name):
-            return name + "_"
-        return name
