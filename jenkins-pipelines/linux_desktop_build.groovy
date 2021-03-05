466350fe443521a125f307d6f4c1ce2278a43c1e
n-wach <nmwachholz@gmail.com>
2021-03-04 21:15:05 -0800

    Run unit tests in python:3.8 docker

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -1,19 +1,21 @@
 pipeline {
-  agent {label "ubuntu && desktop && jenkins-caffe"}
+  agent {
+    label "ubuntu && desktop && docker"
+    docker {
+        image "python:3.8-slim"
+    }
+  }
   stages {
     stage('Install') {
       steps {
         echo "Setting up..."
-        sh "rm -rf venv"
-        sh "rm -f ~/.conservator_config.json"
-        sh "python3 -m venv venv"
-        sh "bash -c 'source venv/bin/activate; pip install --upgrade setuptools; pip install --upgrade wheel; pip install .'"
+        sh "pip install ."
       }
     }
     stage('Formatting Test') {
       steps {
         echo "Checking formatting..."
-        sh "bash -c 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; source venv/bin/activate; black --check .'"
+        sh "black --check ."
       }
     }
     stage('Unit Tests') {
@@ -22,23 +24,19 @@ pipeline {
         CONSERVATOR_URL = 'https://staging.flirconservator.com/'
       }
       steps {
-        echo "Creating tmp directory..."
-        sh "rm -rf /tmp/${BUILD_TAG}-tests"
-        sh "mkdir /tmp/${BUILD_TAG}-tests"
-
         echo "Running unit tests..."
-        dir("/tmp/${BUILD_TAG}-tests/") {
-          sh "bash -c 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; source ${WORKSPACE}/venv/bin/activate; pytest ${WORKSPACE}'"
+        sh "mkdir /tmp/unit-tests"
+        dir("/tmp/unit-tests/") {
+          sh "pytest ${WORKSPACE}"
         }
-
-        echo "Cleaning up tmp directory..."
-        sh "rm -rf /tmp/${BUILD_TAG}-tests"
       }
     }
     stage('Documentation Tests') {
       steps {
         echo "Building docs..."
-        sh "bash -c 'source venv/bin/activate; cd docs; make html'"
+        dir("docs") {
+            sh "make html"
+        }
       }
     }
     stage('Deploy Documentation') {
