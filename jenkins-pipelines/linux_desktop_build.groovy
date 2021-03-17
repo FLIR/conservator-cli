b9a64d45197bddf98fa3f7cc32c9f675441da065
n-wach <nmwachholz@gmail.com>
2021-03-17 15:03:25 -0700

    Push docs from within docker

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -1,59 +1,41 @@
 pipeline {
   agent {
     label "docker"
+    dockerfile {
+      dir "test"
+      label "docker"
+      args "-u root --privileged"
+    }
   }
   stages {
-    stage('Use testing docker') {
-      agent {
-        dockerfile {
-          dir "test"
-          label "docker"
-          args "-u root --privileged"
-          reuseNode true
-        }
+    stage('Install') {
+      steps {
+        echo "Setting up..."
+        sh "pip install --no-cache-dir ."
       }
-      stages {
-        stage('Install') {
-          steps {
-            echo "Setting up..."
-            sh "pip install --no-cache-dir ."
-          }
-        }
-        stage('Formatting Test') {
-          steps {
-            echo "Checking formatting..."
-            sh "black --check ."
-          }
-        }
-        stage('Unit Tests') {
-          steps {
-            echo "Running unit tests..."
-            dir("unit-tests") {
-              sh "pytest $WORKSPACE"
-            }
-          }
-        }
-        stage('Documentation Tests') {
-          steps {
-            echo "Building docs..."
-            dir("docs") {
-              sh "make html"
-            }
-          }
-        }
-        stage('Cleanup') {
-          steps {
-            // Docker needs to run as root, unfortunately that creates some files in the workspace that
-            // the agent wont be able to modify. While we're still root, we need to lower the permissions.
-            // But before we do that, we need to switch to the gh_pages branch (otherwise git gets spooked
-            // by the changes to file modes).
-            sh "git checkout gh_pages"
-            sh "chmod -R 777 ."
-          }
+    }
+    stage('Formatting Test') {
+      steps {
+        echo "Checking formatting..."
+        sh "black --check ."
+      }
+    }
+    stage('Unit Tests') {
+      steps {
+        echo "Running unit tests..."
+        dir("unit-tests") {
+          sh "pytest $WORKSPACE"
         }
       }
     }
-    // Outside of docker container:
+    stage('Documentation Tests') {
+      steps {
+        echo "Building docs..."
+        dir("docs") {
+          sh "make html"
+        }
+      }
+    }
     stage('Deploy Documentation') {
       when {
         branch 'main'
@@ -62,6 +44,7 @@ pipeline {
       steps {
         echo "Deploying..."
         sh "mv docs/_build/html temp/"
+        sh "git checkout gh_pages"
         sh "rm -rf docs/"
         sh "mv temp/ docs/"
         sh "touch docs/.nojekyll"
@@ -69,6 +52,13 @@ pipeline {
         sh "git commit -m 'Build docs for ${BUILD_TAG}' || echo 'Commit failed. There is probably nothing to commit.'"
         sh "git push || echo 'Push failed. There is probably nothing to push.'"
       }
+    }
+  }
+  post {
+    always {
+      // Docker needs to run as root, unfortunately that creates some files in the workspace that
+      // the agent wont be able to modify. While we're still root, we need to lower the permissions.
+      sh "chmod -R 777 ."
     }
   }
 }
