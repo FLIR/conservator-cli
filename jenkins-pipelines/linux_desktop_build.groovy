a41547055220a7237d106c5385da66f93d64d328
Nathan Wachholz <nmwachholz@gmail.com>
2021-03-17 13:48:47 -0700

    Run unit tests in python:3.8 docker (#208)
    * Run unit tests in python:3.8 docker
    
    * Update label
    
    * Install as user
    
    * No cache dir
    
    * Use dockerfile to include git
    
    * Fix agent
    
    * Fix python version
    
    * Run update
    
    * Install as user
    
    * As root
    
    * Install globally
    
    * Remove connection test
    
    * Test different var format
    
    * Don't use tmp
    
    * Fix typo
    
    * Add make for sphinx
    
    * Random change to cause natural rerun
    
    * Fix permissions in post
    
    * Run docs deploy outside test docker
    
    * Fix pipeline
    
    * Reuse same node

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -1,46 +1,57 @@
 pipeline {
-  agent {label "ubuntu && desktop && jenkins-caffe"}
+  agent {
+    label "docker"
+  }
   stages {
-    stage('Install') {
-      steps {
-        echo "Setting up..."
-        sh "rm -rf venv"
-        sh "rm -f ~/.conservator_config.json"
-        sh "python3 -m venv venv"
-        sh "bash -c 'source venv/bin/activate; pip install --upgrade setuptools; pip install --upgrade wheel; pip install .'"
-      }
-    }
-    stage('Formatting Test') {
-      steps {
-        echo "Checking formatting..."
-        sh "bash -c 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; source venv/bin/activate; black --check .'"
-      }
-    }
-    stage('Unit Tests') {
-      environment {
-        CONSERVATOR_API_KEY = credentials('conservator-api-key')
-        CONSERVATOR_URL = 'https://staging.flirconservator.com/'
-      }
-      steps {
-        echo "Creating tmp directory..."
-        sh "rm -rf /tmp/${BUILD_TAG}-tests"
-        sh "mkdir /tmp/${BUILD_TAG}-tests"
-
-        echo "Running unit tests..."
-        dir("/tmp/${BUILD_TAG}-tests/") {
-          sh "bash -c 'export LC_ALL=C.UTF-8; export LANG=C.UTF-8; source ${WORKSPACE}/venv/bin/activate; pytest ${WORKSPACE}'"
+    stage('Use testing docker') {
+      agent {
+        dockerfile {
+          dir "test"
+          label "docker"
+          args "-u root --privileged"
+          reuseNode true
         }
-
-        echo "Cleaning up tmp directory..."
-        sh "rm -rf /tmp/${BUILD_TAG}-tests"
+      }
+      stages {
+        stage('Install') {
+          steps {
+            echo "Setting up..."
+            sh "pip install --no-cache-dir ."
+          }
+        }
+        stage('Formatting Test') {
+          steps {
+            echo "Checking formatting..."
+            sh "black --check ."
+          }
+        }
+        stage('Unit Tests') {
+          steps {
+            echo "Running unit tests..."
+            dir("unit-tests") {
+              sh "pytest $WORKSPACE"
+            }
+          }
+        }
+        stage('Documentation Tests') {
+          steps {
+            echo "Building docs..."
+            dir("docs") {
+              sh "make html"
+            }
+          }
+        }
+        stage('Cleanup') {
+          steps {
+            // Docker needs to run as root, unfortunately that creates some files in the workspace that
+            // the agent can't remove. Not super sure why, but it means that while we're still root, we
+            // need to lower the permissions of everything in the workspace.
+            sh "chmod -R 777 ."
+          }
+        }
       }
     }
-    stage('Documentation Tests') {
-      steps {
-        echo "Building docs..."
-        sh "bash -c 'source venv/bin/activate; cd docs; make html'"
-      }
-    }
+    // Outside of docker container:
     stage('Deploy Documentation') {
       when {
         branch 'main'
