ae2082a44f86e681d595e2cdae3fcde2250e8d4e
Nathan Wachholz <nmwachholz@gmail.com>
2021-03-17 16:04:22 -0700

    Use sshagent for git push (#213)

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -49,7 +49,9 @@ pipeline {
         sh "touch docs/.nojekyll"
         sh "git add docs/"
         sh "git commit -m 'Build docs for ${BUILD_TAG}' || echo 'Commit failed. There is probably nothing to commit.'"
-        sh "git push || echo 'Push failed. There is probably nothing to push.'"
+        sshagent(credentials: ['flir-service-key']) {
+          sh "git push || echo 'Push failed. There is probably nothing to push.'"
+        }
       }
     }
   }
