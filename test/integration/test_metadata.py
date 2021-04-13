16ffef9193c7e646b20ea98256f335d24a670c0f
n-wach <nmwachholz@gmail.com>
2021-04-13 00:44:25 -0700

    Test stubs

---
+++ test/integration/test_metadata.py
@@ -0,0 +1,16 @@
+
+class TestMetadataMedia:
+    def test_upload(self, conservator, test_data):
+        pass
+
+    def test_download(self, conservator, tmp_cwd):
+        pass
+
+
+class TestMetadataDataset:
+    def test_upload(self, conservator, test_data):
+        pass
+
+    def test_download(self, conservator, tmp_cwd):
+        pass
+
