78db7e69ebcc5910ba95c05fa3257d4da4ffe0e9
Nathan Wachholz <nmwachholz@gmail.com>
2021-04-13 17:10:08 -0700

    Test meta classes: TypeProxy, TypeManager, MediaTypeManager (#243)
    * Add TypeProxy tests
    
    * Add TypeManager tests
    
    * Add MediaTypeManager tests
    
    * Reformat
    
    * Fix images vs videos
    
    * Add get_annotations test

--- test/integration/test_media.py
+++ test/integration/test_media.py
@@ -3,6 +3,7 @@ import os
 import pytest
 
 from FLIR.conservator.conservator import UnknownMediaIdException
+from FLIR.conservator.generated.schema import AnnotationCreate
 from FLIR.conservator.util import md5sum_file
 from FLIR.conservator.wrappers import Image, Video
 from FLIR.conservator.wrappers.media import MediaCompare
@@ -176,8 +177,97 @@ def test_get_annotations_empty(conservator, test_data):
 
 
 def test_get_annotations(conservator, test_data):
-    # TODO: Test once we have a way to add annotations
-    pass
+    path = test_data / "jpg" / "cat_0.jpg"
+    media_id = conservator.media.upload(path)
+    conservator.media.wait_for_processing(media_id, check_frequency_seconds=1)
+    image = conservator.images.all().first()
+
+    frame = image.get_frame()
+    annotation = AnnotationCreate(
+        labels=["cat"], bounding_box={"x": 1, "y": 2, "w": 3, "h": 4}
+    )
+    added = frame.add_annotation(annotation)
+
+    annotations = image.get_annotations()
+    assert len(annotations) == 1
+    assert annotations[0].to_json() == added.to_json()
+
+
+def test_from_path(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/CatPics")
+    path = test_data / "jpg" / "cat_0.jpg"
+    media_id = conservator.media.upload(path, collection)
+    conservator.media.wait_for_processing(media_id)
+
+    image = conservator.media.from_path("/My/CatPics/cat_0.jpg")
+    assert image is not None
+    assert image.id == media_id
+
+
+def test_from_string_image_path(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/CatPics")
+    path = test_data / "jpg" / "cat_0.jpg"
+    media_id = conservator.media.upload(path, collection)
+    conservator.media.wait_for_processing(media_id)
+
+    image = conservator.images.from_string("/My/CatPics/cat_0.jpg")
+    assert image is not None
+    assert image.id == media_id
+
+
+def test_from_string_image_id(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/CatPics")
+    path = test_data / "jpg" / "cat_0.jpg"
+    media_id = conservator.media.upload(path, collection)
+    conservator.media.wait_for_processing(media_id)
+
+    image = conservator.images.from_string(media_id)
+    assert image is not None
+    assert image.id == media_id
+
+
+def test_from_string_image_name(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/CatPics")
+    path = test_data / "jpg" / "cat_0.jpg"
+    media_id = conservator.media.upload(path, collection, remote_name="unique.jpg")
+    conservator.media.wait_for_processing(media_id)
+
+    image = conservator.images.from_string("unique.jpg")
+    assert image is not None
+    assert image.id == media_id
+
+
+def test_from_string_video_path(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/Videos")
+    path = test_data / "mp4" / "adas_thermal.mp4"
+    media_id = conservator.media.upload(path, collection)
+    conservator.media.wait_for_processing(media_id)
+
+    video = conservator.videos.from_string("/My/Videos/adas_thermal.mp4")
+    assert video is not None
+    assert video.id == media_id
+
+
+def test_from_string_video_id(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/Videos")
+    path = test_data / "mp4" / "adas_thermal.mp4"
+    media_id = conservator.media.upload(path, collection)
+    conservator.media.wait_for_processing(media_id)
+
+    video = conservator.videos.from_string(media_id)
+    assert video is not None
+    assert video.id == media_id
+
+
+def test_from_string_video_name(conservator, test_data):
+    collection = conservator.collections.create_from_remote_path("/My/Videos")
+    path = test_data / "mp4" / "adas_thermal.mp4"
+    media_id = conservator.media.upload(path, collection, remote_name="unique.mp4")
+    conservator.media.wait_for_processing(media_id)
+
+    video = conservator.videos.from_string("unique.mp4")
+    assert video is not None
+    assert video.id == media_id
 
 
 class TestDownloadMedia:
---
+++ test/integration/test_type_manager.py
@@ -0,0 +1,40 @@
+import pytest as pytest
+
+from FLIR.conservator.wrappers.queryable import InvalidIdException
+
+
+def test_id_exists(conservator):
+    project = conservator.projects.create("Project")
+    assert conservator.projects.id_exists(project.id)
+
+
+def test_id_no_exists(conservator):
+    fake_id = conservator.generate_id()
+    assert not conservator.projects.id_exists(fake_id)
+
+
+def test_from_id(conservator):
+    project_id = conservator.projects.create("Project").id
+
+    project = conservator.projects.from_id(project_id)
+
+    assert project is not None
+    assert project.id == project_id
+
+
+def test_from_id_invalid_raises_error(conservator):
+    invalid_id = "Not an id."
+    with pytest.raises(InvalidIdException):
+        conservator.projects.from_id(invalid_id)
+
+
+def test_from_json(conservator):
+    project = conservator.projects.create("Project")
+
+    json = project.to_json()
+    instance = conservator.projects.from_json(json)
+
+    assert instance is not None
+    assert instance.id == project.id
+    assert instance.name == project.name
+    assert instance.to_json() == json
