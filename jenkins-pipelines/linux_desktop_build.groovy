6899610b70b634a397e36097a8d5e8d61b8eba9e
Neal Butler <nbutler@visgence.com>
2022-08-23 16:25:09 -0600

    Try cleaning before build

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -7,6 +7,12 @@ pipeline {
     }
   }
   stages {
+    stage('Cleanup') {
+      steps {
+        sh "docker image prune"
+        sh "docker system prune -a --filter 'until=120h'"
+      }
+    }
     stage("Install") {
       steps {
         sh "pip install --no-cache-dir -r requirements.txt"
