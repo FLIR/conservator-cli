947df1b256d22ed507f41a0a837bbec775a69b63
Neal Butler <nbutler@visgence.com>
2022-09-01 17:17:49 -0600

    Fixed Conservator URL

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -156,7 +156,7 @@ pipeline {
             script {
               CONSERVATOR_HOST = sh ( script: "ip route list default | sed 's/.*via //; s/ .*//'", returnStdout: true).trim()
               echo "Conservator Host is ${CONSERVATOR_HOST}"
-              sh "python3 -m sgqlc.introspection --exclude-description -H 'authorization: ${env.TEST_API_KEY}' ${CONSERVATOR_HOST}:8080 schema.json"
+              sh "python3 -m sgqlc.introspection --exclude-description -H 'authorization: ${env.TEST_API_KEY}' http://${CONSERVATOR_HOST}:8080/graphql schema.json"
               LATEST_API_VERSION = sh ( script: "md5sum ${SCRIPT_PATH}/schema.json | cut -d ' ' -f 1", returnStdout: true).trim()
               echo "API version on K8S: ${LATEST_API_VERSION}"
               BUILT_API_VERSION = readFile("$WORKSPACE/api_version.txt").trim()
