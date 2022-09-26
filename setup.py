ccf1aae9a92aea25bb5e8ea2521e70d400bec8e9
Neal Butler <nbutler@visgence.com>
2022-09-26 15:38:57 -0600

    Updated requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open("README.md", "r") as fh:
 # Packages only used for developing (pytest, black, etc.) should be placed
 # in requirements.txt instead.
 INSTALL_REQUIRES = [
-    "sgqlc >= 13; python_version>'3.7'",
+    "sgqlc >= 13; python_version>='3.7'",
     "sgqlc == 16.0; python_version<'3.7'",
     "click >= 8",
     "tqdm",
