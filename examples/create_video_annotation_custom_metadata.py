25f0095fc5b73ad72bcad247a096fe00975c498b
Neal Butler <nbutler@visgence.com>
2022-11-28 17:05:09 -0700

    Added example script to update annotation custom metadata

---
+++ examples/create_video_annotation_custom_metadata.py
@@ -0,0 +1,39 @@
+"""
+Demonstrates QA actions on a video/image annotation
+"""
+import json
+
+from FLIR.conservator.conservator import Conservator
+from FLIR.conservator.generated.schema import Mutation, UpdateAnnotationInput
+
+conservator = Conservator.default()
+
+frame_id = input("Please provide a video or image frame id: ")
+
+annotation_id = input("Please provide an annotation id: ")
+
+# Create an object to use as metadata
+metadata_obj = {
+    "metadata": True,
+    "meta": "data",
+    "numberOfFields": 3,
+}
+
+# ... then stringify it
+metadata_string = json.dumps(metadata_obj)
+
+print(f"Adding custom metadata: {metadata_string}")
+
+update_annotation_input = UpdateAnnotationInput(
+    custom_metadata=metadata_string)
+
+annotation = conservator.query(
+    Mutation.update_annotation,
+    frame_id=frame_id,
+    annotation_id=annotation_id,
+    annotation=update_annotation_input,
+)
+
+print("Metadata has been added!")
+
+print(annotation)
