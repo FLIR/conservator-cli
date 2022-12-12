6af3271aadbfaede33b24f14dff88a9445a3b5d4
alex gonzales <acgonzales42@gmail.com>
2022-12-12 16:04:49 -0700

    added fonts, created new logo for cli guide

--- docs/_source/conf.py
+++ docs/_source/conf.py
@@ -13,14 +13,14 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = "Conservator CLI"
-copyright = "2022, Teledyne FLIR LLC"
-author = "Teledyne FLIR LLC"
+project = 'Conservator CLI'
+copyright = '2022, Teledyne FLIR LLC'
+author = 'Teledyne FLIR LLC'
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,31 +29,35 @@ author = "Teledyne FLIR LLC"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.coverage",
-    "sphinx.ext.napoleon",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+templates_path = ['_templates']
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
 
 html_favicon = "_static/favicon.ico"
 html_logo = "_static/logo.webp"
+
+html_theme = 'sphinx_rtd_theme'
+
+# --------------
+
+
+html_show_sphinx = False
+html_show_sourcelink = False
+
+html_logo    = "_static/conservator-cli-guide.svg"
+html_favicon = '_static/favicon.ico'
+html_theme_options = {
+    'logo_only': True,
+}
+
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
