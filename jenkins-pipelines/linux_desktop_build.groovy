0305969d6670b264828550d7c0717ea8378dccee
Neal Butler <nbutler@visgence.com>
2025-10-23 12:48:45 -0700

    WIP

--- jenkins-pipelines/linux_desktop_build.groovy
+++ jenkins-pipelines/linux_desktop_build.groovy
@@ -156,11 +156,10 @@ pipeline {
               CONSERVATOR_HOST = sh ( script: "ip route list default | sed 's/.*via //; s/ .*//'", returnStdout: true).trim()
               echo "Conservator Host is ${CONSERVATOR_HOST}"
               sh "python3 -m sgqlc.introspection --exclude-description -H 'authorization: ${env.TEST_API_KEY}' http://${CONSERVATOR_HOST}:8080/graphql schema.json"
-              sh "cat schema.json"
               def LATEST_API_VERSION = sh ( script: "md5sum schema.json | cut -d ' ' -f 1", returnStdout: true).trim()
               echo "API version on K8S: ${LATEST_API_VERSION}"
               def BUILT_API_VERSION = readFile("$WORKSPACE/api_version.txt").trim()
-              echo "API version in github: ${LATEST_API_VERSION}"
+              echo "API version in github: ${BUILT_API_VERSION}"
               sh "rm schema.json"
               if (LATEST_API_VERSION == BUILT_API_VERSION) {
                 echo "API Versions match ($BUILT_API_VERSION)"
